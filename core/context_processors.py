9fcf685f9920a08888d9cbd64dbc3dc1197cfec1
SAIDOUDIALLO224 <matasaidinfo224@gmail.com>
2026-06-11 18:44:04 +0200

    fix: superadmin voit tous les magasins dans le switcher, defaut=principal

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,9 +1,13 @@
 from .utils import get_current_magasin, get_magasins_visibles
+from .models import Magasin
 
 
 def current_magasin(request):
     ctx = {}
     if request.user.is_authenticated:
         ctx['current_magasin'] = get_current_magasin(request.user)
-        ctx['magasins_liste'] = get_magasins_visibles(request.user)
+        if request.user.is_superuser:
+            ctx['magasins_liste'] = Magasin.objects.all().order_by('-est_principal', 'nom')
+        else:
+            ctx['magasins_liste'] = get_magasins_visibles(request.user)
     return ctx
